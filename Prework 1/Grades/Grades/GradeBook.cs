a9034003bd19333846ec6e5079a5bdc34cc7c706
Daniel Rużewicz <d.ruzew@gmail.com>
2018-01-09 13:14:19 +0100

    Added files of 2 solutions: HelloWorld and Gradebook

---
+++ Prework 1/Grades/Grades/GradeBook.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Grades
+{
+    public class GradeBook
+    {
+        
+        public GradeBook(string name = "There is no name")
+        {
+            _name = name;
+            grades = new List<float>();
+        }
+
+        public void AddGrade(float grade) {
+            if (grade>=0 && grade <= 100)
+            {
+            grades.Add(grade);
+            }
+        }        
+
+        public GradeStatistics ComputeStatistic()
+        {
+            GradeStatistics stats = new GradeStatistics();
+            float  sum = 0f;
+            foreach(float grade in grades)
+            {
+                sum += grade;
+                stats.HighestGrade = Math.Max(stats.HighestGrade, grade);
+                stats.LowestGrade = Math.Min(stats.LowestGrade, grade);
+            }
+            stats.AverageGrade = sum / grades.Count;
+
+            return stats;
+        }
+
+        private string _name;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (!String.IsNullOrEmpty(value))
+                {if (_name!= value)
+                    {
+                        string oldValue = _name;
+                        _name = value;
+                        if (NameChanged != null)
+                        {
+                            NameChangedEventArgs args = new NameChangedEventArgs();
+                            args.OldValue = oldValue;
+                            args.NewValue = value;
+                        NameChanged(this, args); 
+                        }
+                    }
+                    else
+                    {
+                    _name = value;
+                    }
+                }
+            }
+        }
+        public event NameChangedDelegate NameChanged;
+
+        List<float> grades;
+    }
+}
---
+++ Prework 1/Grades/Grades/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+   public class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            GradeBook book = new GradeBook("Daniel's book");
+            book.AddGrade(91f);
+            book.AddGrade(89.1f);
+            book.AddGrade(75f);
+
+            GradeStatistics stats = book.ComputeStatistic();
+          
+            book.NameChanged += OnNameChanged;
+            book.Name = "Scott's book";
+
+            WriteNames(book.Name);
+
+            Console.WriteLine(stats.AverageGrade);
+            Console.WriteLine(stats.HighestGrade);
+            Console.WriteLine(stats.LowestGrade);
+
+            //int number = 20;
+            //WriteBytes(number);
+            //WriteBytes(stats.AverageGrade);
+
+            //Immutable();
+            //ParamsAsValueOrAsReference();
+        }
+
+        private static void OnNameChanged(object sender, NameChangedEventArgs args)
+        {
+            Console.WriteLine("Name changed from {0} to {1}", args.OldValue, args.NewValue);
+        }
+
+        private static void WriteNames(params string[] names)
+        {
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }            
+        }
+
+
+        //private static void WriteBytes(int value)
+        //{
+        //    byte[] bytes = BitConverter.GetBytes(value);
+        //    WriteByteArray(bytes);
+        //}
+
+        //private static void WriteBytes(float value)
+        //{
+        //    byte[] bytes = BitConverter.GetBytes(value);
+        //    WriteByteArray(bytes);
+        //}
+
+        //private static void WriteByteArray(byte[] bytes)
+        //{
+        //    foreach (byte b in bytes)
+        //    {
+        //        Console.Write("0x{0:X} ", b);
+        //    }
+        //    Console.WriteLine();
+        //}
+
+
+        //static void GiveBookAName (ref GradeBook book)
+        //{
+        //    book = new GradeBook();
+        //    book.Name = "Moja ksiazka";
+        //}
+
+        //static void IncreaseNumber(int number)
+        //{
+        //    number = 42;
+        //}
+
+        //private static void Immutable()
+        //{
+        //    string name1 = " Daniel ";
+        //    name1.Trim();
+        //    Console.WriteLine(name1);
+        //    name1 = name1.Trim();
+        //    Console.WriteLine(name1);
+        //    string name2 = "daniel";
+        //    bool AreEqual = name1.Equals(name2, StringComparison.CurrentCultureIgnoreCase);
+        //    Console.WriteLine(AreEqual);
+        //}
+
+        //private static void ParamsAsValueOrAsReference()
+        //{
+        //    GradeBook g1 = new GradeBook();
+        //    GradeBook g2 = g1;
+        //    GiveBookAName(ref g2);
+        //    Console.WriteLine(g2.Name);
+        //    int x1 = 4;
+        //    IncreaseNumber(x1);
+        //    Console.WriteLine(x1);
+        //}
+    }
+}

292bd2ef42d486f7281d2bcbe0c9e0a507fae926
Daniel Rużewicz <d.ruzew@gmail.com>
2018-01-11 19:09:43 +0100

    Exercises from course

---
+++ TabliceKolejkiITP/TabliceKolejkiITP/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TabliceKolejkiITP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tablica = new int[5];
+            for (int i = 0; i < tablica.Length; i++)
+            {
+                tablica[i] = i + 1;
+            }
+            for (int i = tablica.Length -1; i >=0 ; i--)
+            {
+                Console.WriteLine(tablica[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}

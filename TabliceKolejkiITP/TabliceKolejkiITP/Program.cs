6763d01fe4c21c4c9e1ca47d143931ef30e4abc0
Daniel Rużewicz <d.ruzew@gmail.com>
2018-01-11 19:21:50 +0100

    exercise "create array of 5 elements, fill it with numbers. cw sum of numbers in array

--- TabliceKolejkiITP/TabliceKolejkiITP/Program.cs
+++ TabliceKolejkiITP/TabliceKolejkiITP/Program.cs
@@ -11,14 +11,17 @@ namespace TabliceKolejkiITP
         static void Main(string[] args)
         {
             int[] tablica = new int[5];
+            int result = 0;
             for (int i = 0; i < tablica.Length; i++)
             {
                 tablica[i] = i + 1;
+                result += tablica[i];
             }
             for (int i = tablica.Length -1; i >=0 ; i--)
             {
                 Console.WriteLine(tablica[i]);
             }
+            Console.WriteLine("suma = "+result);
 
             Console.ReadKey();
         }
